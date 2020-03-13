cfc9e158f7254379be590ef6ce2846a675665201
Kamil <sadekovk98@mail.ru>
2020-03-13 16:42:28 +0300

    ft: закончил реализовать декстоп , осталось внимательно всё проверить по отступам и начать уже делать адаптив

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	$('.owl-carousel_main_subsect-js').owlCarousel({
+        nav: true,
+        loop: true,
+        responsive: true,
+        dots: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        navText: '',
+        autoplay: true,
+        navContainerClass: 'owl-nav set_down_slider',
+        dotsClass: 'owl-dots set_down_slider',
+	        responsive:{
+	        0:{
+	            items:2
+	        },
+	        567:{
+	            items:3
+	        },
+	        992:{
+	            items:5
+	        }
+	    }
+    });
+});
