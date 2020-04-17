b09f5baad45718f60e4ed200481894377ec2aed7
Kamil Sadekov <50349496+KamilSadekov@users.noreply.github.com>
2020-04-17 16:19:28 +0300

    fixed

--- js/page.js
+++ js/page.js
@@ -2,4 +2,12 @@ $(function(){
 	$('.styler').styler();
 });
 
+$(function() {
+	$(".images-item").click(function(e) {
+	  e.preventDefault();
+	  $(".images-item").removeClass('active-image');
+	  $(this).addClass('active-image');
+	});
+  });
+
 
